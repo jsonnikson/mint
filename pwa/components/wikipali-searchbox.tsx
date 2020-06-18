11262ed6019c3c37e728e5619efa1e396d3d75b6
Joshua Sonnikson <jsonnikson@gmail.com>
2020-06-18 13:20:47 -0400

    :recycle: refactor search box code

--- pwa/components/wikipali-searchbox.tsx
+++ pwa/components/wikipali-searchbox.tsx
@@ -1,58 +1,21 @@
 import React from 'react';
-import { styled, fade } from "@material-ui/core/styles";
-import { InputBase } from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
+import { InputBase, Box } from "@material-ui/core";
+import { Search as SearchIcon } from '@material-ui/icons';
 import { useIntl } from 'react-intl';
-
-const StyledSearchBox = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
-
-
-const SearchBoxIcon = styled('div')(({theme}) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }));
-
-const StyledSearchInput = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-        width: '40ch',
-    }
-}))
+import useStyles from '../styles/styles'
 
 export const WikipaliSearchBox = () => {
-    const intl = useIntl();
+    const intl = useIntl()
+    const classes = useStyles()
     return (
-        <StyledSearchBox>
-            <SearchBoxIcon>
+        <Box className={classes.searchBox}>
+            <Box className={classes.searchBoxIcon}>
                 <SearchIcon />
-            </SearchBoxIcon>
-            <StyledSearchInput
+            </Box>
+            <InputBase
+                className={classes.searchBoxInput}
                 placeholder={intl.formatMessage({id: 'topbar.search'})}
             />
-        </StyledSearchBox>
+        </Box>
     )
 }
--- pwa/styles/styles.ts
+++ pwa/styles/styles.ts
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, fade } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   topbar: {
@@ -16,5 +16,40 @@ export default makeStyles((theme) => ({
       }
     },
   },
-  navbarOpen: {}
+  navbarOpen: {},
+  searchBox: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+  },
+  searchBoxIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  searchBoxInput: {
+    color: 'inherit',
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+        width: '40ch',
+    }
+  }
 }));
