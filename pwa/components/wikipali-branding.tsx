acee7f192afae3e826869b16bcd2cb8661c27d9e
Joshua Sonnikson <jsonnikson@gmail.com>
2020-06-18 12:47:48 -0400

    :lipstick: redesign wikipali branding

--- pwa/components/wikipali-branding.tsx
+++ pwa/components/wikipali-branding.tsx
@@ -1,38 +1,11 @@
 import React from 'react';
-import { styled, fade } from '@material-ui/core/styles';
+import { Box, Typography } from '@material-ui/core';
 
 const wikipaliIconSrc = require('./wikipali-logo.svg')
 
-const WikipaliLogo = styled(() => <img src={wikipaliIconSrc} />)({
-    height: '50px',
-    verticalAlign: 'middle'
-});
-
-const WikipaliText = styled('span')({
-fontFamily: "noto-sans, sans-serif",
-fontStyle: "normal",
-fontWeight: 400,
-fontSize: "20px",
-"&::before": { content: '"wikipali"' }
-});
-
-const StudioText = styled('span')({
-fontFamily: "noto-sans, sans-serif",
-fontStyle: "normal",
-fontWeight: 200,
-fontSize: "20px",
-"&::before": { content: '"studio"' }
-});
-
-const StyledContainer = styled('div')({
-    display: 'inline-flex',
-    alignItems: 'center'
-})
-
 export const WikipaliBranding = () => (
-    <StyledContainer>
-        <WikipaliLogo />
-        <WikipaliText />
-        <StudioText />
-    </StyledContainer>
+    <>
+        <img src={wikipaliIconSrc} />
+        <Typography variant="h6"><b>wikipali</b>studio</Typography>
+    </>
 )
