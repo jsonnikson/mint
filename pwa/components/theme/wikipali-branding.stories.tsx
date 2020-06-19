dfc9122a2fcaba88d5b29b5395d3f6b4d1af5906
Joshua Sonnikson <jsonnikson@gmail.com>
2020-06-19 13:59:32 -0400

    :white_check_mark: add story for wikipali-branding

---
+++ pwa/components/theme/wikipali-branding.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { WikipaliBranding } from './wikipali-branding';
+
+export default {
+  title: 'Theme/Wikipali Branding',
+  component: WikipaliBranding,
+};
+
+export const Basic = () => <WikipaliBranding />
